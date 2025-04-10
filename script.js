edade5352b3ba272a9d35a41eb49809eed6e6d6f
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-04-09 20:59:50 -0400

    Move other functions into Book class

--- script.js
+++ script.js
@@ -17,6 +17,70 @@ class Book {
         this.read = read;
         this.id = crypto.randomUUID();
     }
+
+    addToLibrary() {
+        myLibrary.push(this);
+        console.log(myLibrary);
+    }
+
+    display() {
+        let container = document.createElement('div');
+        container.classList.add('book');
+
+        // All main content declarations
+        let content = document.createElement('div');
+        content.classList.add('content');
+        let titleBox = document.createElement('div');
+        titleBox.classList.add('title');
+        let authorBox = document.createElement('div');
+        authorBox.classList.add('author');
+        let pagesBox = document.createElement('div');
+        pagesBox.classList.add('pages');
+        let readBox = document.createElement('div');
+        
+        // Button declarations and click functions
+        let buttons = document.createElement('div');
+        buttons.classList.add('buttonContainer')
+        let btnRead = document.createElement('img');
+        btnRead.classList.add('btnRead');
+        btnRead.addEventListener('click', () => {
+            this.read = !this.read;
+            readBox.textContent = this.read ? 'Read' : 'Not read'; 
+        });
+        let btnX = document.createElement('img');
+        btnX.classList.add('btnX');
+        btnX.addEventListener('click', () => {
+            this.delete();
+        });
+
+        // Creates the variety of colors in the books
+        let palette = Math.floor(Math.random() * colors.length);
+        container.style.backgroundColor = colors[palette][0];
+        container.style.borderColor = colors[palette][1];        
+
+        // Populates the book with info
+        container.dataset.id = this.id;
+        titleBox.textContent = this.title;
+        authorBox.textContent = `by ${this.author}`;
+        pagesBox.textContent = `${this.pages} pages`;
+        readBox.textContent = this.read ? 'Read' : 'Not read'; 
+
+        // Adds the elements to the DOM
+        container.appendChild(content);
+        content.appendChild(titleBox);
+        content.appendChild(authorBox);
+        content.appendChild(pagesBox);
+        content.appendChild(readBox);
+        container.appendChild(buttons);
+        buttons.appendChild(btnRead);
+        buttons.appendChild(btnX);
+        library.appendChild(container);
+    }
+
+    delete() {
+        let badBook = document.querySelector('[data-id=' + CSS.escape(this.id) + ']');
+        library.removeChild(badBook);
+    }
 }
 
 form.addEventListener('submit', (e) => {
@@ -38,64 +102,6 @@ form.addEventListener('submit', (e) => {
 
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    displayNewBook(book);
-}
-
-function displayNewBook(book) {
-    let container = document.createElement('div');
-    container.classList.add('book');
-
-    // All main content declarations
-    let content = document.createElement('div');
-    content.classList.add('content');
-    let titleBox = document.createElement('div');
-    titleBox.classList.add('title');
-    let authorBox = document.createElement('div');
-    authorBox.classList.add('author');
-    let pagesBox = document.createElement('div');
-    pagesBox.classList.add('pages');
-    let readBox = document.createElement('div');
-    
-    // Button declarations and click functions
-    let buttons = document.createElement('div');
-    buttons.classList.add('buttonContainer')
-    let btnRead = document.createElement('img');
-    btnRead.classList.add('btnRead');
-    btnRead.addEventListener('click', () => {
-        book.read = !book.read;
-        readBox.textContent = book.read ? 'Read' : 'Not read'; 
-    });
-    let btnX = document.createElement('img');
-    btnX.classList.add('btnX');
-    btnX.addEventListener('click', () => {
-        deleteBook(book.id);
-    });
-
-    // Creates the variety of colors in the books
-    palette = Math.floor(Math.random() * colors.length);
-    container.style.backgroundColor = colors[palette][0];
-    container.style.borderColor = colors[palette][1];        
-
-    // Populates the book with info
-    container.dataset.id = book.id;
-    titleBox.textContent = book.title;
-    authorBox.textContent = `by ${book.author}`;
-    pagesBox.textContent = `${book.pages} pages`;
-    readBox.textContent = book.read ? 'Read' : 'Not read'; 
-
-    container.appendChild(content);
-    content.appendChild(titleBox);
-    content.appendChild(authorBox);
-    content.appendChild(pagesBox);
-    content.appendChild(readBox);
-    container.appendChild(buttons);
-    buttons.appendChild(btnRead);
-    buttons.appendChild(btnX);
-    library.appendChild(container);
-}
-
-function deleteBook(id) {
-    badBook = document.querySelector('[data-id=' + CSS.escape(id) + ']');
-    library.removeChild(badBook);
+    book.addToLibrary();
+    book.display();
 }
